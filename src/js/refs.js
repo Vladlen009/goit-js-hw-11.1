89dd2b16f6f075ebb4f0831ba318f396b681f460
Vladyslav Belotskiy <vlad.belotskiy9595@gmail.com>
2022-11-16 20:48:40 +0200

    1

--- src/js/refs.js
+++
@@ -1,8 +0,0 @@
-const refs = {
-  form: document.querySelector('#search-form'),
-  input: document.querySelector('.searh-input'),
-  galleryEl: document.querySelector('.gallery'),
-  loadMore: document.querySelector('.load-more'),
-};
-
-export default refs;
